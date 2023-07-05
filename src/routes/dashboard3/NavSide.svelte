17ce831d8641a7e7016ae30b04d81183d1563949
HY-Leewoojoo <wjlee@hynux.com>
2023-07-05 10:14:40 +0900

    사이드바 테스트 메뉴 추가

--- src/routes/dashboard3/NavSide.svelte
+++ src/routes/dashboard3/NavSide.svelte
@@ -161,18 +161,18 @@
               <hr class="mb-0 navbar-vertical-divider"/>
             </div>
           </div>
-          <!--<a class="nav-link" class:active={$page.url.pathname === '/funcTest/csv'} href="../funcTest/csv">
+          <a class="nav-link" class:active={$page.url.pathname === '/funcTest/csv'} href="../funcTest/csv">
             <div class="d-flex align-items-center">
               <span class="nav-link-icon"><span class="fas fa-cog"></span></span>
               <span class="nav-link-text ps-1">다운로드 테스트</span>
             </div>
-          </a>-->
-       <!--   <a class="nav-link" class:active={$page.url.pathname === '/funcTest/aealias'} href="../funcTest/aealias">
+          </a>
+          <a class="nav-link" class:active={$page.url.pathname === '/funcTest/aealias'} href="../funcTest/aealias">
             <div class="d-flex align-items-center">
               <span class="nav-link-icon"><span class="fas fa-cog"></span></span>
               <span class="nav-link-text ps-1">기기 별칭 변경</span>
             </div>
-          </a>-->
+          </a>
         </li>
         <li class="nav-item">
           <!-- label-->
