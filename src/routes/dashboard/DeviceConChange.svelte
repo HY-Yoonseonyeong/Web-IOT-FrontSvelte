04ee8c5b96df27ffdd43ce8f35bc4cf8c1bc152f
HY-Leewoojoo <wjlee@hynux.com>
2023-10-27 09:50:42 +0900

    대시보드 디바이스명 추가

--- src/routes/dashboard/DeviceConChange.svelte
+++ src/routes/dashboard/DeviceConChange.svelte
@@ -1,18 +1,20 @@
 <script>
-    import {onMount} from "svelte";
+    /**
+     * 대시보다 하단 디바이스 상태 로그
+     */
     import {PUBLIC_API_URL} from '$env/static/public'
+    import {onMount, onDestroy} from "svelte";
     import moment from "moment";
 
+    // const
     let aeiDataList = new Array()
-
-    const url = `${PUBLIC_API_URL}/device/history/change`
 
     /**
      *
      * @returns {Promise<any>}
      */
-    let queryCodChange = async () => {
-        const response = await fetch(url, {
+    const queryConChange = async () => {
+        const response = await fetch(`${PUBLIC_API_URL}/device/history/change`, {
             headers: {
                 "Content-Type": "application/json"
             }
@@ -25,46 +27,44 @@
     }
 
     onMount(() => {
-        queryCodChange().then(data => aeiDataList = data);
+        queryConChange().then(data => aeiDataList = data);
 
         setInterval(async () => {
-            queryCodChange().then(data => aeiDataList = data);
+            queryConChange().then(data => aeiDataList = data);
         }, 10000)
     })
 
-    const get_yyyymmddhhMMss = (datetime) => {
-        let date = new Date(datetime);
-        return date.toISOString().replace('T', ' ').substring(0, 19);
-    }
+    onDestroy(() => {
 
+    })
 
     const getComment = (item) => {
         let comment = item.prev_con + " --> " + item.con
         return comment
     }
-
 </script>
 
 
 <div class="col-md-12">
-  <div class="card z-1" id="recentPurchaseTable" data-list=''>
-    <!-- data-list='{"valueNames":["name","email","product","payment","amount"],"page":7,"pagination":true}'>-->
+  <div class="card z-1">
     <div class="card-header">
       <div class="row flex-between-center">
         <div class="col-6 col-sm-auto d-flex align-items-center pe-0">
           <h5 class="fs-0 mb-0 text-nowrap py-2 py-xl-0">디바이스 상태 로그</h5>
         </div>
         <div class="col-6 col-sm-auto ms-auto text-end ps-0">
-          <!--<div class="d-none" id="table-purchases-actions">
-                                <div class="d-flex"><select class="form-select form-select-sm" aria-label="Bulk actions">
-                                    <option selected="">Bulk actions</option>
-                                    <option value="Refund">Refund</option>
-                                    <option value="Delete">Delete</option>
-                                    <option value="Archive">Archive</option>
-                                </select><button class="btn btn-falcon-default btn-sm ms-2" type="button">Apply</button></div>
-                            </div>-->
+          <div class="d-none" id="table-purchases-actions">
+            <div class="d-flex"><select class="form-select form-select-sm" aria-label="Bulk actions">
+              <option selected="">Bulk actions</option>
+              <option value="Refund">Refund</option>
+              <option value="Delete">Delete</option>
+              <option value="Archive">Archive</option>
+            </select>
+              <button class="btn btn-falcon-default btn-sm ms-2" type="button">Apply</button>
+            </div>
+          </div>
           <div id="table-purchases-replace-element">
-            <!--<button class="btn btn-falcon-default btn-sm" type="button">
+            <button class="btn btn-falcon-default btn-sm" type="button">
               <span class="fas fa-plus" data-fa-transform="shrink-3 down-2"></span>
               <span class="d-none d-sm-inline-block ms-1">New</span>
             </button>
@@ -75,7 +75,7 @@
             <button class="btn btn-falcon-default btn-sm" type="button">
               <span class="fas fa-external-link-alt" data-fa-transform="shrink-3 down-2"></span>
               <span class="d-none d-sm-inline-block ms-1">Export</span>
-            </button>-->
+            </button>
           </div>
         </div>
       </div>
@@ -86,26 +86,26 @@
           <thead class="bg-200 text-900">
           <tr>
             <th class="pe-1 align-middle white-space-nowrap" data-sort="datetime">날짜/시간</th>
-            <th class="pe-1 align-middle white-space-nowrap" data-sort="device">디바이스(AEI)</th>
-            <th class="pe-1 align-middle white-space-nowrap" data-sort="Status">CIN</th>
+            <th class="pe-1 align-middle white-space-nowrap" data-sort="device">디바이스명</th>
+            <th class="pe-1 align-middle white-space-nowrap" data-sort="device">디바이스(RN)</th>
+            <th class="pe-1 align-middle white-space-nowrap" data-sort="Status">CNT</th>
             <th class="pe-1 align-middle white-space-nowrap" data-sort="Value">CON</th>
             <th class="pe-1 align-middle white-space-nowrap" data-sort="Comment">Comment</th>
             <th class="pe-1 align-middle data-table-row-action"></th>
           </tr>
           </thead>
-          <tbody class="list" id="table-purchase-body">
+          <tbody class="list">
           {#each aeiDataList as item, index}
             <tr class="btn-reveal-trigger">
-
               <th class="align-middle white-space-nowrap name">{moment(item.datetime).format("YYYY-MM-DD HH:mm:ss")}</th>
+              <td class="align-middle white-space-nowrap email">{item.alias}</td>
               <td class="align-middle white-space-nowrap email">{item.ae}</td>
-
               {#if item.cin == 'temp'}
                 <td class="align-middle fs-0 white-space-nowrap payment">
                   <span class="badge badge rounded-pill badge-subtle-primary">온도</span>
                   <!--<span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span>-->
                 </td>
-              <td class="align-middle white-space-nowrap product">{item.con}&deg;C</td>
+                <td class="align-middle white-space-nowrap product">{item.con}&deg;C</td>
               {:else if item.cin == 'humid'}
                 <td class="align-middle fs-0 white-space-nowrap payment">
                   <span class="badge badge rounded-pill badge-subtle-success">습도</span>
@@ -156,11 +156,11 @@
           </p>
         </div>
         <div class="col-auto d-flex">
-          <!--<button class="btn btn-sm btn-primary" type="button" data-list-pagination="prev">
+          <button class="btn btn-sm btn-primary" type="button" data-list-pagination="prev">
             <span>Previous</span></button>
           <button class="btn btn-sm btn-primary px-4 ms-2" type="button" data-list-pagination="next">
             <span>Next</span>
-          </button>-->
+          </button>
         </div>
       </div>
     </div>
