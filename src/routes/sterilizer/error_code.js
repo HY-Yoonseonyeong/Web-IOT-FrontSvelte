5c24e219c93b954f11ec42827ade71fe12a83da1
HY-Leewoojoo <wjlee@hynux.com>
2023-12-01 11:40:59 +0900

    멸륜기 에러코드 추가

---
+++ src/routes/sterilizer/error_code.js
@@ -0,0 +1,37 @@
+export const sterilizer_error_code_0 = [
+    "Sys",      /* 0 */
+    "Opt",
+    "bCali0",
+    "bCali1",
+    "bCali2",
+    "bCali3",
+    "bCali4",
+    "E2p",
+    "bAdc0",
+    "bAdc1",
+    "bAdc2",
+    "bAdc3",
+    "bTune0",
+    "bTune1",
+    "bTune2",
+    "bBOut0"    /* 15 */
+]
+
+export const sterilizer_error_code_1 = [
+    "bBOut1",    /* 0 */
+    "bBOut2",
+    "bBOut3",
+    "bBOut4",
+    "bOverP0",
+    "bOverP1",
+    "bOverP2",
+    "bOverP3",
+    "bOverP4",
+    "bOverN0",
+    "bOverN1",
+    "bOverN2",
+    "bOverN3",
+    "bOverN4",
+    "bCom0",
+    "bCom1"     /* 15 */
+]
